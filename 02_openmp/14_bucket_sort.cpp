82623a709419853047aa6ef1e26ac5334b2cb5a8
moritafuto <130618781+moritafuto@users.noreply.github.com>
2023-05-30 18:29:56 +0900

    Update 14_bucket_sort.cpp

--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -1,32 +1,43 @@
 #include <cstdio>
 #include <cstdlib>
 #include <vector>
+#include <omp.h>
 
 int main() {
   int n = 50;
   int range = 5;
   std::vector<int> key(n);
-  for (int i=0; i<n; i++) {
+  for (int i = 0; i < n; i++) {
     key[i] = rand() % range;
-    printf("%d ",key[i]);
+    printf("%d ", key[i]);
   }
   printf("\n");
 
-  std::vector<int> bucket(range,0); 
-  for (int i=0; i<n; i++)
+  std::vector<int> bucket(range, 0);
+  #pragma omp parallel for
+  for (int i = 0; i < n; i++) {
+    #pragma omp atomic
     bucket[key[i]]++;
-  std::vector<int> offset(range,0);
-  for (int i=1; i<range; i++) 
-    offset[i] = offset[i-1] + bucket[i-1];
-  for (int i=0; i<range; i++) {
-    int j = offset[i];
-    for (; bucket[i]>0; bucket[i]--) {
-      key[j++] = i;
-    }
   }
 
-  for (int i=0; i<n; i++) {
-    printf("%d ",key[i]);
+  std::vector<int> offset(range, 0);
+  int sum = 0;
+  for (int i = 0; i < range; i++) {
+    offset[i] = sum;
+    sum += bucket[i];
+  }
+
+  std::vector<int> sorted_key(n);
+  #pragma omp parallel for
+  for (int i = 0; i < n; i++) {
+    int index = offset[key[i]];
+    sorted_key[index] = key[i];
+    #pragma omp atomic
+    offset[key[i]]++;
+  }
+
+  for (int i = 0; i < n; i++) {
+    printf("%d ", sorted_key[i]);
   }
   printf("\n");
 }
